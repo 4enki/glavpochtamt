72af33a7c58bd274efe2872e543bcac592e56f41
Sergey Glazov <yaglazov@gmail.com>
2017-06-04 23:46:01 +0300

    Добавил Главпочтамт

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$("form").submit(function() {
+		$.ajax({
+			url: "mail.php",
+			type: "POST",
+			data: new FormData(this),
+			processData: false,
+			contentType: false
+		}).done(function() {
+			alert("Ready!");
+			console.log("Все письма давно отправлены!");
+		});
+		return false;
+	});
+});
